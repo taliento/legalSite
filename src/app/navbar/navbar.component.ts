612c87c170f6f121a82b12ec82ad5668ed9c971d
Davide Taliento <davidetaliento@gmail.com>
2018-01-08 15:27:25 +0100

    navbar animations

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
-import { navbarAnimation } from '../shared/animations/fade-animation';
+import { navbarFade, navbarBump } from '../shared/animations/navbar-animation';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  animations: [navbarAnimation]
+  animations: [navbarFade, navbarBump]
 })
 export class NavbarComponent implements OnInit {
 
-  state = 'show'
+  state = 'show';
+  bump = 'large';
 
   constructor(public el: ElementRef) { }
 
@@ -22,13 +23,16 @@ export class NavbarComponent implements OnInit {
 
      if(scrollPosition == 0) {
        this.state = 'show';
+       this.bump = 'large';
        return;
      }
 
      if (scrollPosition > componentPosition + 100) {
        this.state = 'hide'
+       this.bump = 'small';
      } else {
-       this.state = 'show'
+       this.state = 'show';
+       this.bump = 'large';
      }
 
    }
--- src/app/shared/animations/navbar-animation.ts
+++ src/app/shared/animations/navbar-animation.ts
@@ -1,9 +1,18 @@
 import { trigger, state, animate, transition, style, query, group } from '@angular/animations';
 
-export const navbarAnimation = trigger('navbarAnimation', [
+export const navbarFade = trigger('navbarFade', [
   state('hide',   style({ opacity: 0, height: '0px' })),
   state('show',   style({ opacity: 1 })),
   // route 'enter' transition
   transition('show => hide', animate('0.5s ease-in-out')),
   transition('hide => show', animate('0.5s ease-in-out'))
+]);
+
+export const navbarBump = trigger('navbarBump', [
+  state('small',   style({ fontSize: '18px'})),
+  state('large',   style({ fontSize: '36px'})),
+  // route 'enter' transition
+  transition('large <=> small', animate(500))
+  // ,
+  // transition('small => large', animate('0.5s ease-in-out'))
 ]);
